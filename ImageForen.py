0815379797639d204987a8972de43d2dff9844db
krdsploit <104392085+krdsploit@users.noreply.github.com>
2022-07-27 09:37:29 -0700

    Update ImageForen.py

--- ImageForen.py
+++ ImageForen.py
@@ -1,26 +1,26 @@
 import numpy as np 
 import cv2 
 
-img1 = cv2.imread("selfie.jpeg")
+img1 = cv2.imread("Photo.jpeg")
 
 jpg_quality1 = 95
 jpg_quality2 = 90
 scale = 15
 
-cv2.imwrite("Selfie_95_ELA.jpeg", img1, [cv2.IMWRITE_JPEG_QUALITY, jpg_quality1])
+cv2.imwrite("Photo_95_ELA.jpeg", img1, [cv2.IMWRITE_JPEG_QUALITY, jpg_quality1])
 
-img2 = cv2.imread("Selfie_95_ELA.jpeg")
+img2 = cv2.imread("Photo_95_ELA.jpeg")
 
 diff1 = scale * cv2.absdiff(img1,img2)
 
-cv2.imwrite("Selfie_90_ELA.jpeg", img2, [cv2.IMWRITE_JPEG_QUALITY, jpg_quality2])
+cv2.imwrite("Photo_90_ELA.jpeg", img2, [cv2.IMWRITE_JPEG_QUALITY, jpg_quality2])
 
-img3 = cv2.imread("Selfie_90_Ela.jpeg")
+img3 = cv2.imread("Photo_90_Ela.jpeg")
 
 diff2 = scale * cv2.absdiff(img2,img3)
 
-cv2.imwrite("Selfie_95_Ela.jpeg", diff1)
-cv2.imwrite("Selfie_90_Ela.jpeg", diff2)
+cv2.imwrite("Photo_95.jpeg", diff1)
+cv2.imwrite("Photo_90_Ela.jpeg", diff2)
 
 cv2.imshow("Result[1]", diff1)
 cv2.imshow("Result[2]", diff2)
